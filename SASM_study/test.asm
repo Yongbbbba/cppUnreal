a76658b3c6cf882a55de700423eea772d427eedc
Yongbbbba <lisagamer123@gmail.com>
2021-03-09 21:02:00 +0900

    어셈블리 언어 입문 : 사칙연산

--- SASM_study/test.asm
+++ SASM_study/test.asm
@@ -4,7 +4,71 @@ section .text
 global CMAIN
 CMAIN:
     mov rbp, rsp; for correct debugging
-    PRINT_STRING msg
+    
+    GET_DEC 1, al
+    GET_DEC 1, num
+    
+    ;PRINT_DEC 1, al
+    ;NEWLINE
+    ;PRINT_DEC 1, num
+    
+    ; 더하기 연산
+    ; add a,b ( a = a + b)
+    ; a는 레지스터 or 메모리
+    ; b는 레지스터 or 메모리 or 상수
+    ; 단, a,b 모두 메모리여서는 안된다
+    
+    ; 빼기 연산
+    ; sub a,b ( a = a - b)
+    ; 더하기와 동일
+    
+    ; 곱하기 연산
+    ; 곱하기 연산은 더하기 빼기보다 훨씬 복잡한 연산임
+    ; 곱하기 연산에 사용되는 레지스터가 따로 있음
+    ; mul reg
+    ; - mul bl => al * bl
+    ; -- 연산 결과를 ax에 저장
+    ; - mul bx => ax * bx
+    ; -- 연산 결과는 dx(상위 16비트) ax(하위 16비트)에 저장
+    ; -- mul ebx => eax * ebx
+    
+    ; ex ) 5 * 8은?
+    mov ax, 0
+    mov al, 5
+    mov bl, 8
+    mul bl
+    PRINT_DEC 2, ax
+    NEWLINE
+    
+    ; 나누기 연산
+    ; div reg
+    ; - div bl = > ax / bl
+    ; -- 연산 결과는 al(몫) ah(나머지)
+    
+    ; ex) 100 /3 은? 
+    mov ax, 100
+    mov bl, 3
+    div bl
+    
+    PRINT_DEC 1, al
+    NEWLINE
+    mov al, ah ; ah는 PRINT_DEC로 출력을 못해서 al로 옮겨서 출력 
+    PRINT_DEC 1, al
+    NEWLINE
+    
+    add al, 1; ; 레지스터 + 상수
+    PRINT_DEC 1, al 
+    
+    add al, [num] ; 레지스터 + 메모리, 괄호 안하면 메모리 주소임
+    PRINT_DEC 1, al
+    NEWLINE
+    
+    mov bl,3 
+    add al, bl ; 레지스터 + 레지스터
+    PRINT_DEC 1, al
+    NEWLINE
+    
+    add [num], byte 1
     
     xor rax, rax
     ret
@@ -19,18 +83,12 @@ CMAIN:
     ; [변수이름] [크기] [초기값]
     ; [크기] db(1) dw(2) dd(4) dq(8)  define ~
     
-section .data
-    a db 0x11, 0x11, 0x11, 0x11    ; [0x11]
-    
-    b dd 0x12345678 ; 까보면 리틀엔디언 방식으로 저장됨
-    ; 장단점이 교차한다
-    ; - 리틀 엔디언 : 캐스팅에 유리하다, 예를 들어 1바이트만 추출하려고 하면 맨 앞에 있는 것만 가지고 오면 된다.
-    ; - 빅 엔디언 : 숫자 비교에 유리하다
-    
+; section .data
+   
     
     ; 초기화 되지 않은 데이터
     ; [변수이름] [크기] [개수]
     ; [크기] resb(1) resw(2) resd(4) resq(8) reserve일 것으로 추정
     
 section .bss
-    e resb 10
+    num resb 1
