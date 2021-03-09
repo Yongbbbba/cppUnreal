90b4dc40bd0b853553864b9acfe6013c78ec1d45
Yongbbbba <lisagamer123@gmail.com>
2021-03-09 09:04:51 +0900

    어셈블리 입문 : 문자와 엔디언

--- SASM_study/test.asm
+++ SASM_study/test.asm
@@ -4,20 +4,7 @@ section .text
 global CMAIN
 CMAIN:
     mov rbp, rsp; for correct debugging
-    ; 8 bit = 1byte
-    ; 16 bit = 2 byte = 1 word
-    ; 32 bit = 4 byte = 2 word = 1 dword(double-word)
-    ; 64 bit = 8 byte = 4 word = 1 qword (quad-word)
-    
-    mov eax, 0x1234
-    mov rbx, 0x12345678
-    mov cl, 0xff
-    
-    mov al, 0x00
-    mov rax, rdx
-    ; 메모리 <-> 레지스터
-    mov rax, a
-    
+    PRINT_STRING msg
     
     xor rax, rax
     ret
@@ -26,18 +13,24 @@ CMAIN:
     ; 변수는 그냥 데이터를 저장하는 바구니 [ ]
     ; - 처음에 바구니 사용하겠다 선언 (이름과 크기 지정)
     
+    ; 메모리에는 구분할 수 있도록 주소(번지수)가 있다
+    
     ; 초기화 된 데이터
     ; [변수이름] [크기] [초기값]
     ; [크기] db(1) dw(2) dd(4) dq(8)  define ~
     
 section .data
-    a db 0x11    ; [0x11]
-    b dw 0x2222
-    c dd 0x33333333
-    d dq 0x4444444444444444
+    a db 0x11, 0x11, 0x11, 0x11    ; [0x11]
+    
+    b dd 0x12345678 ; 까보면 리틀엔디언 방식으로 저장됨
+    ; 장단점이 교차한다
+    ; - 리틀 엔디언 : 캐스팅에 유리하다, 예를 들어 1바이트만 추출하려고 하면 맨 앞에 있는 것만 가지고 오면 된다.
+    ; - 빅 엔디언 : 숫자 비교에 유리하다
+    
     
     ; 초기화 되지 않은 데이터
     ; [변수이름] [크기] [개수]
     ; [크기] resb(1) resw(2) resd(4) resq(8) reserve일 것으로 추정
+    
 section .bss
     e resb 10
