64cb756469e8ca01dfadf988f20c18fc2d1a39fd
Yongbbbba <lisagamer123@gmail.com>
2021-03-18 00:11:00 +0900

    포인터 : 배열 기초

--- CPP_Study/CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study/CPP_Study.cpp
@@ -42,5 +42,41 @@ int main()
 	monster_1->attack = 20; 
 	monster_1->defence = 2;
 
+	// 포인터와 참조는 자유자재로 변환 가능하다
+	StatInfo& monster_2 = *(monsters + 2);
+	monster_2.hp = 200;
+	monster_2.attack = 30;
+	monster_2.defence = 3;
+
+	// [주의] 이거는 완전 다른 의미다
+	// 배열에 직접 접근한 것이 아니고 배열의 값만 복사해서 새로운 변수를 만든 것. 아래의 작업을 해도 배열의 값은 변동이 없다
+	StatInfo temp = *(monsters + 2);
+	temp.hp = 300;
+	temp.attack = 30;
+	temp.defence = 3;
+
+	// 이를 조금 더 자동화한다! 
+	for (int i = 0; i < 10; i++)
+	{
+		StatInfo& monster = *(monsters + i);
+		monster.hp = 100 * (i+1);
+		monster.attack = 10 * (i+1);
+		monster.defence = (i+1);
+	}
+
+	// 근데 *(monsters + i) 너무 불편하고 가독성이 떨어진다.. .더 편한 방법? 
+	// 인덱스!
+	// 배열은 0번부터 시작. N번째 인덱스에 해당하는 데이터에 접근하려면 배열이름[N]
+	// *(monsters + i) = monsters[i]
+
+	// 배열 초기화 문법 몇 가지 
+	int numbers[5] = {}; // 0으로 초기화, 반복문이랑 이렇게 초기화하는 것이 성능상으로는 차이가 없다. 어차피 어셈블리 코드 까보면 하나하나 0넣어주고 있다
+	int numbers1[10] = { 1,2,3,4,5 }; // 설정값은 설정한 애들로 초기화 되고, 나머지 값들은 0으로 초기화
+	int numbers2[] = { 1,2,31,2,3,21,45,1, };
+	
+	char helloStr[] = { 'H', 'e', 'l', 'l', 'o', '\0' };
+	cout << helloStr << endl; 
+
+
 	return 0;
 }
