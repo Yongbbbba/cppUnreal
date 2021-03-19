cf742b0de39819930bef9f6dbd5b22cfa1dffbbd
Yongbbbba <lisagamer123@gmail.com>
2021-03-19 18:13:17 +0900

    포인터 : 다차원 배열

--- CPP_Study/CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study/CPP_Study.cpp
@@ -2,93 +2,47 @@
 
 using namespace std;
 
-// 오늘의 주제 : 로또 번호 생성기
-
-void Swap(int& a, int& b);
-void Sort(int numbers[], int size);
-void ChooseLotto();
+// 오늘의 주제 : 다차원 배열
+// 2차원 배열을 만들어도 메모리는 선형으로 쭉 할당되어있음. 다루기 편하게하는 기능이라고 보면 됨
 
 int main()
 {
-	// 1) Swap 함수 만들기 
-	int a = 1;
-	int b = 2;
-	Swap(a, b);
-	// a = 2, b = 1
-	//cout << a << " " << b << endl;
+	int apartment2D[2][5] = {
+		{4,2,3,4,1},
+		{1,1,5,2,2},
+	};
 
-	// 2) 정렬 함수 만들기 (작은 숫자가 먼저 오도록 정렬)
-	int numbers[6] = { 1,42,3,15,5,6 };
-	Sort(numbers, 6);   // sizeof(numbers) / sizeof(int) => 배열 사이즈 구하는 법
-	// {1,3,5,6,15,42}
-	//for (auto num : numbers) cout << num << " ";
+	for (int floor = 0; floor < 2; floor++)
+	{
+		for (int room = 0; room < 5; room++)
+			// apartment2D + (floor * 20) + 4 * room과 아래는 동일
+			cout << apartment2D[floor][room] << " ";
+		cout << endl;
+	}
 
-	// 3) 로또 번호 생성기
-	ChooseLotto();
-	
+	int apartment1D[10] = { 4,2,3,4,1,1,1,5,2,2 };
+	for (int floor = 0; floor < 2; floor++)
+	{
+		for (int room = 0; room < 5; room++)
+		{
+			int index = (floor * 5) + room;
+			int num = apartment1D[index];
+			cout << num << " ";
+		}
+		cout << endl;
+	}
+
+	// 2차 배열은 언제 사용할까? 대표적으로 2D 로그라이크 맵
+	// 갈 수 있는 길과 그럴 수 없는 길을 2차원 배열로 표시
+	int map[5][5] =
+	{
+		{1,1,1,1,1},
+		{1,0,1,1,1},
+		{0,0,0,0,1},
+		{1,1,1,0,0},
+		{1,1,1,1,1},
+	};
+
 	return 0;
 }
 
-void Swap(int& a, int& b)
-{
-	int temp = a;
-	a = b;
-	b = temp;
-}
-
-void Sort(int numbers[], int size)
-{
-	// 선택정렬 사용
-	for (int i = 0; i < size; i++)
-	{
-		int min = numbers[i];
-		int min_idx=i;
-		for (int j = i+1; j < size; j++)
-		{
-			if (numbers[j] < min)
-			{
-				min = numbers[j];
-				min_idx = j;
-			}
-		}
-
-		int temp = numbers[i];
-		numbers[i] = numbers[min_idx];
-		numbers[min_idx] = temp;
-	}
-}
-
-void ChooseLotto()
-{
-	// TODO : 랜덤으로 1~45 사이의 숫자 6개를 골라주세요! (단, 중복이 없어야함)
-	// 순서대로 출력
-
-	srand(time(0)); // 랜덤 시드 설정
-	int numbers[6];
-	int temp;
-	int i = 0;
-	while (i != 6)
-	{
-		temp = (rand() % 46) + 1;
-		bool check = true;
-		// 중복 체크하기
-		for (int j = 0; j < i; j++)
-		{
-			if (numbers[j] == temp)
-			{
-				check = false;
-				break;
-			}
-		}
-
-		if (check)
-		{
-			numbers[i] = temp;
-			i++;	
-		}
-	}
-	
-	Sort(numbers, 6);
-	for (auto num : numbers) cout << num << " ";
-	cout << endl;
-}
