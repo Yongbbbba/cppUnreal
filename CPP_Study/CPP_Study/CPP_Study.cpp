cd7ce85b7697a5d432105064606aa8f0baa1e6b5
Yongbbbba <lisagamer123@gmail.com>
2021-03-21 23:24:54 +0900

    객체지향 여행 : 객체지향의 시작

--- CPP_Study/CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study/CPP_Study.cpp
@@ -1,12 +1,69 @@
 ﻿#include <iostream>
-#include "Test1.h"
 using namespace std;
 
-// 오늘의 주제 : 파일 분할 관리
+// 오늘의 주제 : 객체 지향의 시작
+// 객체 지향 = 객체 
+// 객체란? 플레이어, 몬스터, gameroom .. 
 
+// Knight를 설계해보자 
+// - 속성(데이터) : hp, attack, y, x
+// - 기능(동작) : Move, Attack, Die
+
+// class와 struct는 종이 한 장 차이
+
+class Knight
+{
+public:
+	// 멤버 함수 선언
+	void Move(int y, int x);
+	void Attack();
+	void Die();
+
+public:
+	// 멤버 변수
+	int hp;
+	int attack;
+	int posY;
+	int posX;
+};
+
+void Knight::Move(int y, int x)
+{
+	posY = y;
+	posX = x;
+	cout << "Move" << endl;
+}
+
+void Knight::Attack()
+{
+	cout << "Attack : " << attack << endl;
+}
+
+void Knight::Die()
+{
+	hp = 0;
+	cout << "Die" << endl;
+}
+
+// instantiate 객체를 만든다! 
 int main()
 {
-	Test_1();
+	
+	Knight k1;
+	k1.hp = 100;
+	k1.attack = 10;
+	k1.posY = 0;
+	k1.posX = 0;
+
+	Knight k2;
+	k2.hp = 80;
+	k2.attack = 5;
+	k2.posY = 1;
+	k2.posX = 1;
+
+	k1.Move(2, 2);
+	k1.Attack();
+	k1.Die();
 
 	return 0;
 }
--- CPP_Study/CPP_Study/Test1.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-void Test_1();
-void Test_2();
-void Test_3();
