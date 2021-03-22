6fe0671c6e5799b38d12aaff8ce6f7b49199732c
Yongbbbba <lisagamer123@gmail.com>
2021-03-23 00:20:25 +0900

    객체 지향 여행 : 상속성

--- CPP_Study/CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study/CPP_Study.cpp
@@ -1,135 +1,136 @@
 ﻿#include <iostream>
 using namespace std;
 
-// 오늘의 주제 : 생성자와 소멸자
+// 오늘의 주제 : 상속성
 
-// [생성자(Constructor) 소멸자(Destructor)
-// 클래스에 '소속'된 함수들을 멤버 함수라고 함
-// 이중에서 굉장히 특별한 함수 2종이 있는데 바로 [시작]과 [끝]을 알리는 함수들
-// - 시작(탄생) -> 생성자 호출(여러 개 존재 가능)
-// - 끝(소멸) -> 소멸자 호출 (오직 1개만)
-// 
-// [암시적(inplicit) 생성자]
-// 생성자를 명시적으로 만들지 않으면,
-// 아무 인자도 받지 않는 [기본 생성자]가 컴파일러에 의해 자동으로 만들어짐 + 복사 생성자도 컴파일러가 만들어줌
-// -> 그러나 우리가 명시적(Explicit)으로 아무 생성자 하나 만들면, 
-// 자동으로 만들어지던 [기본 생성자]는 더 이상 만들어지지 않음!!!!! 
+// 객체지향 (OOP Object Oriented Programming)
+// - 상속성
+// - 은닉성
+// - 다형성
 // 
 // 
-// 클래스는 일종의 설계도
-// class를 정의만 해놨다고 해서 메모리 공간을 차지하는 것이 아니다. (함수의 경우도 마찬가지였었음)
-class Knight
+// class는 일종의 설계도
+
+// 메모리
+// [ [ Player ] ]
+// [    Knight   ]
+
+// 상속(Inheritance) 부모 -> 자식에게 유산을 물려주는 것
+
+// 생성자(N) /소멸자(1)
+
+// 생성자는 탄생을 기념해서 호출되는 함수? 
+// - Knight를 생성하면 -> Player의 생성자? Knight의 생성자?
+// -> 솔로몬의 선택 ! 그냥 둘다 호출하자! 
+
+// RPG 게임에서 최상위 객체를 만들어주면서 시작
+// GameObject
+// - Creature
+// -- Player, Monster, Mpc, Pet
+// - Projectile
+// -- Arrow, Fireball
+// - Env
+
+// Item
+// - weapon
+// -- Swrod, Bow
+// - Armor
+// -- Helmet. Boots
+
+class Player
 {
 public:
-	// [1] 기본 생성자 (인자가 없음)
+
+	Player()
+	{
+		_hp = 0;
+		_attack = 0;
+		_defence = 0;
+		cout << "Player() 기본 생성자 호출" << endl;
+	}
+
+	Player(int hp)
+	{
+		_hp = hp;
+		_attack = 0;
+		_defence = 0;
+		cout << "Player(int) 기타 생성자 호출" << endl;
+	}
+
+	~Player()
+	{
+		cout << "~Player() 소멸자 호출" << endl;
+	}
+
+
+	void Move() { cout << "Player Move 호출" << endl; }
+	void Attack() { cout << "Player Attack 호출" << endl; }
+	void Die() { cout << "Player Die 호출" << endl; }
+
+public:
+	int _hp;
+	int _attack;
+	int _defence;
+};
+
+class Knight : public Player
+{
+public:
 	Knight()
 	{
-		cout << "knight() 기본 생성자 호출" << endl;
+		/*
+			선(먼저)처리 영역
+			- 여기서 Player() 생성자 호출
+		*/
 
-		_hp = 100;
-		_attack = 10;
-		_posX = 0;
-		_posY = 0;
+		_stamina = 100;
+		cout << "Knight() 기본 생성자 호출" << endl;
+
 	}
 
-	// [2] 복사 생성자 (자기 자신의 클래스 참조 타입을 인자로 받음)
-	// (일반적으로 '똑같은' 데이터를 지닌 객체가 생성되길 기대한다
-	// 명시를 안해줘도 암묵적으로 컴파일러가 복사 생성자를 만들어준다.
-	Knight(const Knight& knight)
+	Knight(int stamina) : Player(100)  // 선처리 영역의 생성자를 지정할 수 있다.
 	{
-		_hp = knight._hp;
-		_attack = knight._attack;
-		_posX = knight._posX;
-		_posY = knight._posY;
+		cout << "Knight(int) 기타 생성자 호출" << endl;
+		_stamina = stamina;
 	}
 
-	// [3] 기타 생성자
-
-	// 이 중에서 인자를 1개만 받는 [기타 생성자]를
-	// [타입 변환 생성자]라고 부른다
-
-	// 명시적인 용도로만 사용할 것 !  - explicit
-	explicit Knight(int hp)
-	{
-		cout << "Knight(int) 생성자 호출" << endl;
-		_hp = hp;
-		_attack = 10;
-		_posX = 0;
-		_posY = 0;
-	}
-
-	// 소멸자
 	~Knight()
 	{
 		cout << "~Knight() 소멸자 호출" << endl;
+
+		/*
+			일종의 후(나중에)처리 영역이 존재
+			- 여기서 ~Player() 소멸자를 호출
+		*/
 	}
 
-	// 멤버 함수 선언
-	void Move(int y, int x);
-	void Attack();
-	void Die();
-
+	// 재정의: 부모님의 유산을 거부하고 새로운 이름으로 만든?
+	void Move() { cout << "Knight Move 호출" << endl; }
 public:
-	// 멤버 변수
-	int _hp;
-	int _attack;
-	int _posY;
-	int _posX;
+	int _stamina;
 };
 
-void Knight::Move(int y, int x)
+class Mage : public Player
 {
-	_posY = y;
-	_posX = x;
-	cout << "Move" << endl;
-}
+public:
 
-void Knight::Attack()
-{
-	cout << "Attack : " << _attack << endl;
-}
+public:
+	int _mp;
+};
 
-void Knight::Die()
-{
-	_hp = 0;  // 사실 어셈블리 확인해보면  this->_hp = 0 이랑 똑같음 !! 
-	cout << "Die" << endl;
-}
-
-// instantiate 객체를 만든다! 
 int main()
 {
-	  
-	Knight k1;
-	k1._hp = 100;
-	k1._attack = 10;
-	k1._posY = 0;
-	k1._posX = 0;
+	Knight k(100);
 
-	Knight k2(k1);
+	k._hp = 100;
+	k._attack = 10;
+	k._defence = 5;
+	//k._stamina = 50;
 
-	// **************************************************************
-	// 아래 두 가지는 같은 동작을 하는 것 같지만 완전히 다른 코드이다.
-	Knight k3 = k1;  // 복사 생성자 호출
-
-	Knight k4; // 기본 생성자 호출
-	k4 = k1;
-	// *************************************************************
-	k1.Move(2, 2);
-	k1.Attack();
-	k1.Die();
-
-	// 암시적 형변환 -> 컴파일러가 알아서 바꿔치기
-	int num = 1;
-
-	float f = (float)num;  // 명시적 < 우리가 코드로 num을 float 바구니에 넣으라고 주문하고 있음
-	double d = num;  // 암시적 << 별말 안했는데 컴파일러가 알아서 처리하고 있음
+	k.Move();
+	// k.Player::Move();  // 실무에서 거의 쓸 일이 없음. 이렇게 쓸 일이 있다는 것은 애초에 잘못 만든 것..
+	k.Attack();
+	k.Die();
 	
-	Knight k5;
-	k5 = 1;  // 얘는 어떻게 처리가 되나...? 암시적 형변환 : 아까 만들었던 (3) 기타 생성자로 호출이 된다
-	// 그래서 타입 변환 생성자라는 이름이 있는 것이다
-	// 이런 암시적 형변환을 막기 위하여 생성자 앞에 explicit 키워드를 붙여준다
-	
-
-
 	return 0;
-}
+}  
