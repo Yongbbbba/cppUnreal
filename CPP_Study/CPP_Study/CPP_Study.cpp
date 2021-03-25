6a1d421ca81c20c4ee792b5119f750a8046151b5
Yongbbbba <lisagamer123@gmail.com>
2021-03-25 23:47:27 +0900

    동적할당 : 얕은 복사 vs 깊은 복사 #2  - 매우 심오하고 어려운 내용.. 실무하면서 마주하고 꼭 다시 공부해야할 내용

--- CPP_Study/CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study/CPP_Study.cpp
@@ -18,86 +18,105 @@ public:
 	{
 		cout << "Pet(const Pet&)" << endl;
 	}
+
+	Pet& operator=(const Pet& pet)
+	{
+		cout << "operator=(const pet&)" << endl;
+		return *this;
+	}
 };
 
-class RabbitPet : public Pet
+class Player
 {
+public:
+	Player()
+	{
+		cout << "Player()" << endl;
+	}
 
+	// 복사 생성자
+	Player(const Player& player)
+	{
+		cout << "Player(const Player&)" << endl;
+		_level = player._level;
+	}
+
+	// 복사 대입 연산자
+	Player& operator=(const Player& player)
+	{
+		cout << "operator=(const Player&)" << endl;
+		_level = player._level;
+		return *this;
+	}
+
+public:
+	int _level = 0;
 };
 
-class Knight
+class Knight : public Player
 {
 public:
 	Knight()
 	{
-		_pet = new Pet();
+	
 	}
 
-	Knight(const Knight& knight)
+	Knight(const Knight& knight) : Player(knight), _pet(knight._pet)  // 이렇게 안하면 기본 생성자 호출됨
 	{
-		_hp = knight._hp;
-		_pet = new Pet(*knight._pet);  // 깊은 복사
-	}
+		cout << "Knight(const Knight&)" << endl;
 
-	Knight& operator=(const Knight& knight)
-	{
 		_hp = knight._hp;
-		_pet = new Pet(*knight._pet);  // 깊은 복사
-		return *this;
 	}
 
 	~Knight()
 	{
-		delete _pet;
 	}
 
 public:
 	int _hp = 100;
-	// Pet _pet;  // 만약 Pet이 4000바이트가 넘는 크기를 가지고 있다면 Knight 객체를 만들 떄마다 이만한 크기가 덩달아 생김
-	// 보통 클래스의 멤버 변수로 다른 클래스 타입의 변수를 가져야하는 경우에 포인터 형태로 갖고 있는 경우가 일반적
-	Pet* _pet;
+	
+	Pet _pet;
 };
 
 
 int main()
 {
-
-	// Pet* pet = new Pet();
-
-	Knight knight;  // 기본 생성자
+	Knight knight; // 기본 생성자
 	knight._hp = 200;
-	// knight._pet = pet;
+	knight._level = 99;
 
-	// 아래 두 방법이 같아 보이지만 미묘하게 다름
+	cout << "---------------복사 생성자----------------" << endl;
 	Knight knight2 = knight;  // 복사 생성자
-	// Knight knight3(knight);
 
-	Knight knight3; // 기본 생성자
-	knight3 = knight;  // 복사 대입 연산자
-
-	// [복사 생성자] + [복사 대입 연산자]
-	// 둘 다 안만들어주면 컴파일러가 '암시적으로' 만들어준다
-
-	// 중간 결론) 컴파일러가 알아서 잘 만들어준다?
-	// 수고하세요~ 다음 주제 넘어갈까요?  << NO 
-
-	// [ 얕은 복사 Shallow Copy ] 
-	// 멤버 데이터를 비트열 단위로 '똑같이' 복사 (메모리 영역 값을 그대로 복사)
-	// 복사 생성자나 복사 대입 연산자를 통해서 knight1이 가지고 있던 것을 그대로 복사해버림 - 심지어 pet 포인터가 가리키고 있던 주소까지도..
-	// 마치 같은 주소 공간을 공유하는 느낌처럼 되어버림.
-	// 포인터는 주소값 바구니 -> 주소값을 똑같이 복사 -> 동일한 객체를 가리키는 상태가 됨
-	// 	   이런 경우 발생되는 문제는 knight1이 소멸하게 되면 같은 주소 공간을 쓰고 있던 knight2, knight3이 소멸하면서 double free 문제가 발생하면서 crash가 발생할 수 있다는 것이다
+	cout << "---------------복사 대입 연산자----------------" << endl;
+	Knight knight3;  // 기본 생성자
+	knight3 = knight; // 복사 대입 연산자
+	
+	// 실험)
+	// - 암시적 복사 생성자 Steps
+	// 	   1) 부모 클래스의 복사 생성자 호출
+	// 	   2) 멤버 클래스의 복사 생성자 호출 (멤버 변수에 클래스 타입의 변수가 있을 경우)
+	// 	   3) 멤버가 기본 타입일 경우 메모리 복사 (얕은 복사 방식) 
 	// 
-	// Stack : Knight1 [ hp  0x1000  ]  -> Heap  0x1000 Pet [   ]
-	// Stack : Knight2 [ hp  0x1000  ]  
-	// Stack : Knight3 [ hp  0x1000  ]  
+	// - 명시적 복사 생성자 Steps
+	// 1) 부모 클래스의 기본 생성자 호출
+	// 2) 멤버 클래스의 기본 생성자 호출
 
-	// [ 깊은 복사 Deep Copy ]
-	// 멤버 데이터가 참조(주소) 값이라면, 데이터를 새로 만들어준다 (원본 객체가 참조하는 대상까지 새로 만들어서 복사)
-	// 포인터는 주소값 바구니 -> 새로운 객체를 생성 -> 상이한 객체를 가리키는 상태가 됨
-	// Stack : Knight1 [ hp  0x1000  ]  -> Heap  0x1000 Pet [   ]
-	// Stack : Knight2 [ hp  0x2000  ]  -> Heap 0x2000 Pet [   ]
-	// Stack : Knight3 [ hp  0x3000  ]  -> Heap 0x3000 Pet [   ]
+	// - 암시적 복사 대입 연산자 Steps
+	// 	   1) 부모 클래스의 복사 대입 연산자 호출
+	// 	   2) 멤버 클래스의 복사 대입 연산자 호출 (멤버 변수에 클래스 타입의 변수가 있을 경우)
+	// 	   3) 멤버가 기본 타입일 경우 메모리 복사 (얕은 복사 방식) 
+	// - 명시적 복사 대입 연산자 Steps
+	// 1) 알아서 해주는거 없음
+
+	// 왜 이렇게 혼란스러울까?
+	// 객체를 '복사'한다는 것은 두 객체의 값들을 일치시키려는 것
+	// 따라서 기본적으로 얕은 복사 (Shallow Copy) 방식으로 동작한다.
+
+	// 명시적 복사 -> [모든 책임]을 프로그래머한테 위임하겠다는 의미
+
+	// 우리가 커스터마이징해서 깊은 복사를 구현하려고 한다면.. 기본적으로 처리되는 부분들이 누락되기 때문에
+	// 이것들을 제대로 챙겨주지 않으면 반쪽자리 복사가 되게 된다.
 
 	return 0;
 }
