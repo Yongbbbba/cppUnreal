b8a42e36f32d5a1a8d5a9131b4eaf5c474f496f0
Yongbbbba <lisagamer123@gmail.com>
2021-03-26 23:25:44 +0900

    실습 : TextRPG(OOP) #2

--- CPP_Study/CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study/CPP_Study.cpp
@@ -1,10 +1,20 @@
 ﻿#include <iostream>
+#include "Game.h"
+
 using namespace std;
 
 // 오늘의 주제 : TextRPG #4	
 
 int main()
 {
-	
+	Game game;
+	game.Init();
+
+	while (true)
+	{
+		game.Update();
+	}
+
+
 	return 0; 
 }
