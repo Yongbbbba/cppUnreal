549c972424113a28b5109928bd3fee6f738d96a8
Yongbbbba <lisagamer123@gmail.com>
2021-03-27 12:03:25 +0900

    모던 c++ : nullptr - 함수 포인터 공부하고 다시 볼 것

--- CPP_Study/CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study/CPP_Study.cpp
@@ -3,71 +3,60 @@
 
 using namespace std;
 
-// 오늘의 주제 : 중괄호 초기화 { }	
+// 오늘의 주제 : nullptr	
 
 class Knight
 {
-public:
-	Knight()
-	{
 
-	}
-	~Knight()
-	{
-
-	}
-	Knight(initializer_list<int> li)
-	{
-		cout << "Knight(initailizer_list)" << endl;
-	}
 };
+
+Knight* FindKnight()
+{
+	// TODO,
+
+	return nullptr;
+}
+
+void Test(int a)
+{
+	cout << "Test(int)" << endl;
+}
+
+void Test(void* ptr)
+{
+	cout << "Test(*)" << endl;
+}
+
+// Nullptr 구현
+
+class NullPtr
+{
+public:
+	template<typename T>
+	operator T* () const
+	{
+		return 0;
+	}
+	
+};
+
 int main()
 {	
-	// 중괄호 초기화 
-	int a = 0;
-	int b(0);
-	int c{ 0 };
+	// NULL은 그냥 0임
+	// define NULL 0
+	
+	int* ptr = NULL; // 0 NULL
 
-	Knight k1;
-	Knight k2 = k1;   // 복사 생성자 호출 (대입 연산자가 아님)
-	// Knight k2; // 기본 생성자 호출
-	// k2 = k1;  // 이 경우가 대입 연산자 사용
+	// 1) 오동작 방지 가능
+	Test(0);
+	Test(NULL);
+	Test(nullptr);
 
-	Knight k3{ k1 };  
+	// 2) 가독성
+	auto knight = FindKnight();
 
-	int arr[] = { 1,2,3,4 };
-
-	vector<int> v1;
-	v1.push_back(1);
-	v1.push_back(2);
-
-	vector<int> v2(10, 1);
-
-
-	// 중괄호 초기화 
-	// 1) vector 등 container와 잘 어울린다
-	vector<int> v3{ 1,2,3,4 };
-	// 2) 축소 변환 방지
-	int x = 0;
-	double y = x; // 가능한
-	// double z{ x };  // 축소 변환 방지해서 빌드가 안됨
-
-	// 3) Bonus
-	Knight k4(); // 이게 기본생성자 호출하라는 의미가 아님, 함수를 선언해준 것과 모양이 같음
-	Knight k5{}; // 중괄호를 사용하면 기본생성자 호출을 하게 해줌
-
-	Knight k5{ 1,2,3,4,5 };
-
-	// 취향 차이임
-	// 
-	// 괄호 초기화 ()를 기본으로 간다
-	// - 전통적인 C++ (거부감이 없음)
-	// - vector 등 특이한 케이스에 대해서만 {} 사용
-
-	// 중괄호 초기화 {}를 기본으로 간다
-	// - 초기화 문법의 일치화
-	// - 축소 변환 방지
-
+	auto whoami = nullptr;  // nullptr_t 타입
+	
 
 	return 0; 
 }
