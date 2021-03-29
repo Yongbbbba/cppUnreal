375b7f4e0488f408381330e0835b3530bca4ca51
Yongbbbba <lisagamer123@gmail.com>
2021-03-29 13:33:58 +0900

    콜백 함수 : 함수 객체

--- CPP_Study/CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study/CPP_Study.cpp
@@ -2,95 +2,104 @@
 
 using namespace std;
 
-// 오늘의 주제 : 함수 포인터	
+// 오늘의 주제 : 함수 객체
 
-
-int Test(int a, int b)
+void HelloWorld(void)
 {
-	cout << "Test" << endl;
+	cout << "Hello World" << endl;
+}
 
-	return 0;
+void HelloNumber(int number)
+{
+	cout << "Hello Number" << number << endl;
 }
 
 class Knight
 {
 public:
-
-	// 정적 함수
-	// 객체와 관련은 없음
-	static void HelloKnight()
+	void AddHp(int addHp)
 	{
-
+		_hp += addHp;
 	}
-
-	// 멤버 함수 
-	int GetHp(int, int)
-	{
-		cout << "GetHp()" << endl;
-		return _hp;
-	}
-public:
-	int _hp;
+private:
+	int _hp = 100;
 };
 
+class Functor
+{
+public:
+	void operator() ()
+	{
+		cout << "Functor Test" << endl;
+		cout << _value << endl;
+	}
 
-// typedef의 진실
-// typedef 왼쪽 오른쪽값 -> 오른쪽 (커스텀 타입 정의)
+	bool operator()(int num)
+	{
+		cout << "Functor Test" << endl;
+		_value += num;
+		cout << _value << endl;
 
-// 정확히는 왼쪽/오른쪽 기준이 아니라,
-// [선언 문법]에서 typedef을 앞에다 붙이는 쪽
+		return true;
+	}
 
-typedef int INTEGER;
-typedef int* POINTER;
-typedef int FUNC(int, int);
-typedef int ARRAY[20];
+private:
+	int _value = 0;
+};
 
-typedef int(*PFUNC)(int, int); // 함수 포인터 
-typedef int(Knight::*PMEMFUNC)(int, int);  // 멤버 함수 포인터 
+class MoveTask
+{
+public:
+	void operator()()
+	{
+		// TODO
+		cout << "해당 좌표로 플레이어 이동" << endl;
+	}
 
+public:
+	int _playerId;
+	int _posX;
+	int _posY;
+};
 
 int main()
 {	
+	// 함수 객체 (Functor) : 함수처럼 동작하는 객체
+	// 함수 포인터 선언
+	void (*pfunc)(void);
 
-	// 함수 포인터 구성 요소
-	// 1) 포인터		*
-	// 2) 변수 이름     fn
-	// 3) 데이터 타입	함수(인자로 int 2개를 받고, int 1개를 반환)
+	// 동작을 넘겨줄 때 유용하다
+	pfunc = &HelloWorld;
+	(*pfunc)();
 
-	FUNC t;  // 일반적인 변수의 선언처럼 메모리에 올라가는 개념이 아님. 
-
-
-	int(*fn)(int, int);  // 함수 포인터 만들기
-
-	//typedef int(FUNC_TYPE)(int, int);
-	// using FUNC_TYPE =  int(*)(int, int);
-
-	//FUNC_TYPE* fn;
-
-	fn = Test;
-	// fn = &Test;   //  & 생략 가능 (C언어 와의 호환성 때문)
-
-	fn(1, 2);
-
-	(*fn)(1, 2);
-
-	// 위 문법으로 [전역 함수 / 정적 함수/만 담을 수 있다 (호출 규약이 동일한 애들)
-	//fn = &Knight::GetHp;
+	// 함수 포인터의 단점
+	// 1) 동일한 시그니처를 가진 함수만 포인터 변수에 넣을 수 있다
+	// pfunc = &HelloNumber;  // 사용 불가
+	// 2) 상태를 가질 수 없다
 	
-	Knight k1;
-	k1.GetHp(1, 1);
-
-	PMEMFUNC mfn;
-
-	mfn = &Knight::GetHp;  // 멤버 함수에는 & 반드시 붙여야함
 	
-	(k1.*mfn)(1, 2);
+	// 이럴 때 함수 객체를 사용하면 단점을 해결할 수 있다
 
-	Knight* k2 = new Knight();
-	((*k2).*mfn)(1, 1);
-	(k2->*mfn)(1, 1);   // 실무에서 잘 사용 안하는 문법이긴 함.. 서버 시간에 사용하기는 것
+	// 함수 객체 
+	// [함수처럼 동작]하는 객체
+	// () 연산자 오버로딩
 
-	delete k2;
+	Functor functor;
+
+	functor();
+	bool ret = functor(3);
+
+	// MMO에서 함수 객체를 사용하는 예시
+	// 클라 <-> 서버
+	// 서버 : 클라가 보내준 네트워크 패킷을 받아서 처리
+	// ex) 클라 : 나 (5, 0) 좌표로 이동시켜줘!
+	MoveTask task;
+	task._playerId = 100;
+	task._posX = 5;
+	task._posY = 0;
+
+	// 나중에 여유 될 때 일감을 실행한다
+	task();
 
 	return 0; 
 }
