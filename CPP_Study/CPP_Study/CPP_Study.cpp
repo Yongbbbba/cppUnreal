550e358bf722382af04a4f1c79990c5306b1b662
Yongbbbba <lisagamer123@gmail.com>
2021-04-02 23:07:44 +0900

    STL : map #1

--- CPP_Study/CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study/CPP_Study.cpp
@@ -2,54 +2,91 @@
 #include <vector>
 #include <list>
 #include <deque>
+#include <map>
 
 using namespace std;
 
-// 오늘의 주제 : deque
+// 오늘의 주제 : map
+class Player
+{
+public:
+	Player() : _playerId(0) {}
+	Player(int playerId) : _playerId(playerId) {}
 
-// deque : double-ended queue 데크
-// 실무에서 vector와 list를 많이 쓰지 이건 많이 쓰진 않음
-// [    3 3]
-// [1 1 1 2]
-// [2      ]
+public:
+	int _playerId;
+};
 
 
 int main()
 {
-	// 시퀀스 컨테이너(Sequence Container)
-	// 데이터가 삽입 순서대로 나열되는 형태
-	// vector list deque
+	// 연관 컨테이너
 	
-	//deque<int> dq;
+	vector<Player*> v;
 
-	//dq.push_back(1);
-	//dq.push_front(2);
-	//cout << dq[0] << endl;
+	// 10만 명 입장
+	for (int i = 0; i < 100000; i++)
+	{
+		Player* p = new Player(i);
+		v.push_back(p);
+	}
 
-	// vector와 마찬가지로 배열 기반으로 동작
-	// 다만 메모리 할당 정책이 다르다
+	// 5만 명이 퇴장
+	// 랜덤으로 나간다고 생각
+	for (int i = 0; i < 50000; i++)
+	{
+		int randIndex = rand() % v.size();
+
+		Player* p = v[randIndex];
+		delete p;
+
+		v.erase(v.begin() + randIndex);
+	}
+
+	// (ID = 2만 플레이어)가 (ID = 1만 player)를 공격하고 싶어요
+	// Q) ID = 1만인 player를 찾아주세요
+	// A) 찾아본다. vector는 처음부터 끝까지 원하는 값이 나올 때까지 찾아봐야함! 
+
+	// vector, list의 치명적인 단점
+	// -> 원하는 조건에 해당하는 데이터를 빠르게 찾을 수 [없다]
+
+	// map :  균형 이진 트리 (AVL)
+	// - 노드 기반
 	
-	vector<int> v(3, 1);
-	deque<int> dq(3, 1);
+	class Node
+	{
+	public:
+		Node* _left;
+		Node* _right;
+		// DATA
+		pair<int, Player*> _data;
+		/*int _key;
+		Player* _value;*/
+	};
+
+	// (Key, value)
+	map<int, int> m;
+
+	// 10만명
+	for (int i = 0; i < 100000; i++)
+	{
+		m.insert(pair<int, int>(i, i * 100));
+	}
+
+	// 5만명 퇴장
+	for (int i = 0; i < 50000; i++)
+	{
+		int randomValue = rand() % 50000;
+
+		// Erase By Key
+		m.erase(randomValue);
+	}
+
+	// Q) ID = 1만인 Player 찾고 싶다!
+	// A) 매우 빠르게 찾을 수 있음
 	
-	v.push_back(2);
-	v.push_back(2);
-
-	dq.push_back(2);
-	dq.push_back(2);
-
-	dq.push_front(3);
-	dq.push_front(3);
-
-	// - deque의 동작 원리
-	// - 중간 삽입/삭제 (BAD / BAD)
-	// - 처음/끝 삽입/삭제 (GOOD / GOOD)
-	// - 임의 접근 (GOOD),
-
-	dq[3] = 10;
-	cout << dq[3] << endl;  // -> 임의 접근 연산이 빠르게 일어나기 때문애 이런 문법이 있는 것.
-	// iterator 까보면 몇 동 몇 호를 찾아가는 느낌으로 return을 한다. 그래서 약간 벡터와 비슷하게 바로 랜덤 엑세스가 가능해진다.
-	// 임의 접근을 빠르게 해놔서 중간 삽입/삭제가 어렵다.
+	m.find(10000);
+	
 
 	return 0;
 }
