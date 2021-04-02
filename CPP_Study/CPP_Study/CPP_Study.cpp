76ffee3f24ec01cb841be57be747a68252a02678
Yongbbbba <lisagamer123@gmail.com>
2021-04-02 12:47:51 +0900

    STL : deque - 동작 원리에 대한 이해가 약간 떨어짐

--- CPP_Study/CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study/CPP_Study.cpp
@@ -1,44 +1,55 @@
 ﻿#include <iostream>
 #include <vector>
 #include <list>
+#include <deque>
 
 using namespace std;
 
-// 오늘의 주제 : override, final
+// 오늘의 주제 : deque
 
-class Player
-{
-public:
-	virtual void Attack()
-	{
-		cout << "Player!" << endl;
-	}
-};
+// deque : double-ended queue 데크
+// 실무에서 vector와 list를 많이 쓰지 이건 많이 쓰진 않음
+// [    3 3]
+// [1 1 1 2]
+// [2      ]
 
-class Knight : public Player
-{
-public:
-	// 재정의(override)
-	// 이것만 보아서는 최초로 virtual을 선언한 곳이 어딘지 알 수 없음 -> override
-	
-	virtual void Attack() override  // final -> 내 자식들은 더이상 이 함수를 오버라이딩 할 수 없다.
-	{
-		cout << "Knight!" << endl;
-	}
-
-	// 오버로딩(overloading) : 함수 이름의 재사용
-	void Attack(int a)
-	{
-
-	}
-};
 
 int main()
 {
-
-	Player* player = new Knight();
-
-	player->Attack();
+	// 시퀀스 컨테이너(Sequence Container)
+	// 데이터가 삽입 순서대로 나열되는 형태
+	// vector list deque
 	
+	//deque<int> dq;
+
+	//dq.push_back(1);
+	//dq.push_front(2);
+	//cout << dq[0] << endl;
+
+	// vector와 마찬가지로 배열 기반으로 동작
+	// 다만 메모리 할당 정책이 다르다
+	
+	vector<int> v(3, 1);
+	deque<int> dq(3, 1);
+	
+	v.push_back(2);
+	v.push_back(2);
+
+	dq.push_back(2);
+	dq.push_back(2);
+
+	dq.push_front(3);
+	dq.push_front(3);
+
+	// - deque의 동작 원리
+	// - 중간 삽입/삭제 (BAD / BAD)
+	// - 처음/끝 삽입/삭제 (GOOD / GOOD)
+	// - 임의 접근 (GOOD),
+
+	dq[3] = 10;
+	cout << dq[3] << endl;  // -> 임의 접근 연산이 빠르게 일어나기 때문애 이런 문법이 있는 것.
+	// iterator 까보면 몇 동 몇 호를 찾아가는 느낌으로 return을 한다. 그래서 약간 벡터와 비슷하게 바로 랜덤 엑세스가 가능해진다.
+	// 임의 접근을 빠르게 해놔서 중간 삽입/삭제가 어렵다.
+
 	return 0;
 }
