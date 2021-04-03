93b4763a104419146b2312b36d33fae2f9184f9a
Yongbbbba <lisagamer123@gmail.com>
2021-04-03 13:13:54 +0900

    STL : algorithm

--- CPP_Study/CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study/CPP_Study.cpp
@@ -4,12 +4,30 @@
 #include <deque>
 #include <map>
 #include <set>
+#include <algorithm>
+
 using namespace std;
 
 // 오늘의 주제 : 연습 문제
 
 int main()
 {
+	// 자료구조 (데이터를 저장하는 구조)
+	// 알고리즘 (데이터를 어떻게 사용할 것인가?)
+
+	// find
+	// find_if
+	// count
+	// count_if
+	// all_of
+	// any_of
+	// none_of
+	// for_each
+	// 
+	// remove
+	// remove_if
+
+
 	srand(static_cast<unsigned int>(time(nullptr)));
 	vector<int> v;
 
@@ -27,30 +45,16 @@ int main()
 		vector<int>::iterator it;
 
 		// TODO
-		for (auto& num : v)
+		vector<int>::iterator itFind = find(v.begin(), v.end(), number);
+		if (itFind == v.end())
 		{
-			if (num == number)
-			{
-				found = true;
-				break;
-			}
-		}
-		for (it = v.begin(); it != v.end(); ++it)
-		{
-			if ((*it) == number)
-				break;
-		}
-		cout << "1번 답 : found = " << found << endl;
-
-		if (it == v.end())
-		{
-			cout << "찾지 못 했습니다" << endl;
+			// 못찾았음
+			cout << "못 찾음" << endl;
 		}
 		else
 		{
-			cout << "1번 답 : it = " << *it << endl;
+			cout << "찾았음" << endl;
 		}
-		int a = 3;
 	}
 
 	// Q2) 11로 나뉘는 숫자가 벡터에 있는지 체크하는 기능 (bool, 첫 등장 iterator)
@@ -59,57 +63,96 @@ int main()
 		vector<int>::iterator it;
 
 		// TODO
-		for (auto& num : v)
+		// 함수 객체 활용
+		struct CanDivideBy11
 		{
-			if (num % 11 == 0)
+			bool operator() (int n)
 			{
-				found = true;
-				break;
+				return (n % 11) == 0;
 			}
-		}
-		for (it = v.begin(); it != v.end(); ++it)
-		{
-			if ((*it) % 11 == 0)
-				break;
-		}
+		};
 
-		
-		cout << "2번 답 : found = " << found << endl;
-
-		if (it == v.end())
+		auto itFind = find_if(v.begin(), v.end(), CanDivideBy11());  // 람다식을 사용하면 더 간단하게 할 수 있음
+		if (itFind == v.end())
 		{
-			cout << "찾지 못 했습니다" << endl;
+			// 못찾았음
+			cout << "못 찾음" << endl;
 		}
 		else
 		{
-			cout << "2번 답 : it = " << *it << endl;
+			cout << "찾았음" << endl;
 		}
-		
 	}
 	
 	// Q3) 홀수인 숫자의 개수는? (count)
 	{
-		int count = 0;
-
-		// TODO
-		for (auto& num : v)
+		struct IsOdd
 		{
-			if (num % 2)
-				count++;
-		}
-		cout << "3번 답 : count = " << count << endl;
+			bool operator() (int n)
+			{
+				return (n % 2) != 0;
+			}
+		};
+		int n = count_if(v.begin(), v.end(), IsOdd());
+		cout << "총 : " << n << "개" << endl;
+
+		// 모든 데이터가 홀수 입니까>
+		bool b1 = all_of(v.begin(), v.end(), IsOdd());
+		//홀수인 데이터가 하나라도 있습니까?
+		bool b2 = any_of(v.begin(), v.end(), IsOdd());
+		// 모든 데이터가 홀수가 아닙니까?
+		bool b3 = none_of(v.begin(), v.end(), IsOdd());
 	}
 
 	// Q4) 벡터에 들어가 있는 모든 숫자들에 3을 곱해주세요!
 	{
-		for (auto& num : v)
-			num *= 3;
+		struct MultiplyBy3
+		{
+			void operator()(int& n)
+			{
+				n = n * 3;
+			}
+		};
 
-		for (auto num : v)
-			cout << num << endl;
+		for_each(v.begin(), v.end(), MultiplyBy3());
 	}
 
+	// 홀수인 데이터를 일괄 삭제
+	{
+		for (vector<int>::iterator it = v.begin(); it != v.end();)
+		{
+			if (*it % 2 != 0)
+				it = v.erase(it);
+			else
+				++it;
+		}
+		v.clear();
+		v.push_back(1);
+		v.push_back(4);
+		v.push_back(3);
+		v.push_back(5);
+		v.push_back(8);
+		v.push_back(2);
 
+		struct IsOdd
+		{
+			bool operator() (int n)
+			{
+				return (n % 2) != 0;
+			}
+		};
+
+
+		// 1 4 3 5 8 2
+		// 4 8 2 5 8 2 
+		//remove(v.begin(), v.end(), 4);
+
+		vector<int>::iterator it = remove_if(v.begin(), v.end(), IsOdd());
+		// remove_if는 삭제가 이뤄지는게 아니고, 홀수가 아닌 값을 앞쪽으로 move시킨다.
+		// 그래서 4 8 2 5 8 2라는 묘한 값으로 벡터가 변해있고, 반환되는 이터레이터 이후부터는 5 8 2 값이 들어가는 것이다
+		// 그래서 아래와 같이 그 이터레이터 이후의 값을 제거해줘야 원하는 벡터의 값들을 얻을 수 있게 된다.
+		v.erase(it, v.end());
+	}
 
 	return 0;
 }
