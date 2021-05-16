3635c809e99bff246429dfbc293c1499c355076c
Yongbbbba <lisagamer123@gmail.com>
2021-05-16 14:30:05 +0900

    C# : 딕셔너리

--- CSharp_study/CSharp/Program.cs
+++ CSharp_study/CSharp/Program.cs
@@ -6,27 +6,46 @@ namespace CSharp
 
     class Program
     {
-    
-      
+
+        class Monster
+        {
+            public int id;
+            public Monster(int id) { this.id = id; }
+        }
+   
         static void Main(string[] args)
         {
-            // List <- 동적 배열
+            // 딕셔너리
+
+            // 리스트의 단점
             List<int> list = new List<int>();
+            // 캐릭터, 몬스터 등에는 ID 식별자를 부여하게 된다.
+            // 103이라는 ID를 가진 몬스터를 공격하려고 할 때 그 몬스터를 어떻게 찾을 것인가? 이걸 리스트로 관리하고 있었다면?
+
+            // 특정 Key를 가지고 value에 빠르게 접근하는 방법이 필요
+            // Dictionary
+
+            Dictionary<int, Monster> dic = new Dictionary<int, Monster>();
+
             
-            for (int i=0; i<5; i++)
+            //dic.Add(1, new Monster(1));
+
+            //dic[5] = new Monster(5);
+
+            for (int i=0; i<10000; i++)
             {
-                list.Add(i);
+                dic.Add(i, new Monster(i));
             }
 
-            // 삽입 삭제
-            list.Insert(2, 999); // 2번 인덱스에 999를 넣어주세요
-         
-            list.RemoveAt(0); // 해당 인덱스의 값을 지우는 것
-            list.Remove(999);  // 값을 지우는 것. 여러 개 있으면 처음 만나는 애를 삭제 함 (얘는 O(N)의 복잡도를 가짐. 앞에서부터 탐색을 해야하니까)
+            Monster mon;
+            bool found = dic.TryGetValue(20000, out mon); // 바로 키값을 넣어서 확인하다가 없는 키값을 찾을라고하면 에러가 나기 때문에 TryGetvalue를 활용 
 
-            list.Clear();  // 전체 삭제
-          
+            dic.Remove(7777);
+            dic.Clear();
 
+            // Dictionary는 HashTable 기법을 사용
+            // 메모리 손해는 있지만 메모리를 내조고, 성능을 취함
+            
         }
 
     }
