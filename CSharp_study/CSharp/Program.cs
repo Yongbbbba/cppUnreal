46090865b48198bd4ab86a40c4e49944bf7cdb15
Yongbbbba <lisagamer123@gmail.com>
2021-05-16 15:24:56 +0900

    C# : delegate

--- CSharp_study/CSharp/Program.cs
+++ CSharp_study/CSharp/Program.cs
@@ -5,43 +5,40 @@ namespace CSharp
 {
 
     class Program
-    { 
-        // 객체지향 -> 은닉성
-        class Knight
+    {
+
+        // 업체 사장 - 사장님의 비서를 통해 연락
+        // 우리의 연락처/용건 남겨서 거꾸로 (콜백) 연락을 달라고 .. 
+
+        delegate int OnClicked();  // 함수가 아니라 형식임
+        // delegate -> 형식은 형식인데, 함수 자체를 인자로 넘겨주는 그런 형식
+        // 반환: int 입력:void
+        // Onclicked이 delegate 형식의 이름이다!
+
+        // UI
+        static void ButtonPressed(OnClicked clickedFuntion /* 함수 자체를 인자로 넘겨주고 */)
         {
-            //protected int hp;
+            // PlayAttack();
+            // 이렇게 하면 UI 코드와 게임 로직 코드가 엉키게되는 경우가 생길 수 있음. 실제 게임에서 이 부분을 나누는게 중요함
 
-            //public int Hp
-            //{
-            //    get { return hp; }
-            //    set { hp = value; }
-            //}
-
-            public int Hp { get; set; } //  아래 코드를 프로퍼티 자동완성을 통해 매우 간단히 작성할 수 있음
-
-            private int _hp;
-            public int GetHp() { return _hp; }
-            public void SetHp(int value) { _hp = value; }
-            
-            // Getter Get 함수
-            //public int GetHp() { return hp; }
-            // Setter Set 함수
-            //public void SetHp(int hp) { this.hp = hp; }
+            // 함수를 호출();
+            clickedFuntion();
         }
-        
+
+        static int TestDelegate()
+        {
+            Console.WriteLine("Hello Delegate");
+            return 0;
+        }
+     
         static void Main(string[] args)
         {
 
-            // 프로퍼티
-            Knight knight = new Knight();
-            //knight.hp = 100; 
-            //knight.SetHp(100);
+            // delegate 대리자
+            // 중요하고, 자주 사용되는 문법임
 
-            int hp = knight.Hp;
-            knight.Hp = 100;
+            ButtonPressed(TestDelegate);
 
-          
-            
         }
 
     }
