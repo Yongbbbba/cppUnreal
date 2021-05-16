bad3f1739467d3719504ff5602aeff3a620f5900
Yongbbbba <lisagamer123@gmail.com>
2021-05-16 15:11:32 +0900

    C# : 프로퍼티

--- CSharp_study/CSharp/Program.cs
+++ CSharp_study/CSharp/Program.cs
@@ -4,65 +4,41 @@ using System.Collections.Generic;
 namespace CSharp
 {
 
-    abstract class Monster // 추상 클래스 만들기, 개념적으로만 존재하는... 
-    {
-        //public virtual void Shout() { }
-        public abstract void Shout(); // 추상 클래스를 상속했을 때 그 자식 클래스는 반드시 Shout를 구현해야함. 아니면 에러를 내뱉는다
-    }
-
- 
-
-    class Orc : Monster
-    {
-        public override void Shout()
-        {
-            Console.WriteLine("록타르 오가르!");
-        }
-    }
-
-    //abstract class Flyable
-    //{
-    //    public abstract void Fly();
-    //}
-    // 다중 상속의 단점 : 죽음의 다이아몬드
-    //class FlyableOrc : Orc, Flyable  // c# 에서는 다중 상속이 불가능함, c++에서는 가능
-    // 인터페이스는 물려줘도 구현부는 물려주지 않는 방법을 통해 다이아몬드 문제를 해결할 수 있음 => interface라는 문법 사용
-    interface IFlyable
-    {
-        void Fly();
-    }
-    class FlyableOrc : Orc, IFlyable
-    {
-        public void Fly()
-        {
-            Console.WriteLine("나는 날고 있어요");
-        }
-    }
-
-    static void DoFly(IFlyable flyable)
-    {
-
-    }
-
-
-
-    class Skeleton : Monster
-    {
-        public override void Shout()
-        {
-            Console.WriteLine("꾸에에엑");
-        }
-    }
-
-    
-
     class Program
     { 
+        // 객체지향 -> 은닉성
+        class Knight
+        {
+            //protected int hp;
+
+            //public int Hp
+            //{
+            //    get { return hp; }
+            //    set { hp = value; }
+            //}
+
+            public int Hp { get; set; } //  아래 코드를 프로퍼티 자동완성을 통해 매우 간단히 작성할 수 있음
+
+            private int _hp;
+            public int GetHp() { return _hp; }
+            public void SetHp(int value) { _hp = value; }
+            
+            // Getter Get 함수
+            //public int GetHp() { return hp; }
+            // Setter Set 함수
+            //public void SetHp(int hp) { this.hp = hp; }
+        }
+        
         static void Main(string[] args)
         {
-            // 인터페이스, 추상 클래스
 
-            // Monster monster = new Monster(); // 추상 클래스는 인스턴스를 만들 수 없음 
+            // 프로퍼티
+            Knight knight = new Knight();
+            //knight.hp = 100; 
+            //knight.SetHp(100);
+
+            int hp = knight.Hp;
+            knight.Hp = 100;
 
           
             
