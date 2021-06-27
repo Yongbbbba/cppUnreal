ec4f699652553488c1ad6e0d72bebbe9b28938eb
Yongbbbba <lisagamer123@gmail.com>
2021-06-28 01:02:38 +0900

    C++ server: stomp allocator

--- cpp_server/Server/GameServer/GameServer.cpp
+++ cpp_server/Server/GameServer/GameServer.cpp
@@ -76,11 +76,30 @@ public:
 
 int main()
 {
-	//Knight* knight = new Knight();
-	//delete knight;
-	// 이런 식으로 메모리 할당과 해제를 하다보면 메모리 단편화 문제가 생겨서, 메모리 풀 같은 것들을 만들어서 메모리 낭비를 막기 위한
-	// 프로그래머들의 노력이 있었다. 하지만 컴파일러가 성능이 좋아져서 이렇게 new, delete를 사용해도 잘 관리가 되고 있어서 실제 프로젝트에서 이런 식으로 처리하는 곳이 늘어났다.
-	// 하지만 메모리풀을 몰라서 사용 못 하는거랑 알고도 필요가 없어서 안하는거랑은 천지차이이기 때문에 이를 숙지하고 있을 필요가 있다.
+	// 가상 메모리 기본
+	
+	// 유저 레벨(메모장, 롤, 서버)
+	// --------------
+	// 커널레벨(OS Code)
+
+	// 2GB [                          ]
+	// 운영체제는 보통 페이지 단위로 메모리를 관리한다.
+	// 2GB / 4KB[r][w][rw][X][][][][][][][][][]
+
+	SYSTEM_INFO info;
+	::GetSystemInfo(&info); 
+
+	info.dwPageSize; // 4kb(0x1000)
+	info.dwAllocationGranularity; // 64kb, 메모리할당할 때 이 주소 단위로 반환한다.
+
+	// win api를 사용해서 메모리 할당, 해제하기
+	// new, delete 보다 더 정밀함 옵션을 줘서 할당, 해제 가능해진다.
+	int* test = (int*)::VirtualAlloc(NULL, 4, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+	*test = 100;
+	::VirtualFree(test, 0, MEM_RELEASE);
+
+	*test = 200; // -> delete 했을 때와 다르게 크래쉬가 발생한다. 
+
 	Knight* knight = xnew<Knight>(100);
 
 	xdelete(knight);
