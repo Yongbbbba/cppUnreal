d9e9b2f93d5bb0b1a4677e85b8f327dcbbe2040e
yongbbbba <lisagamer123@gmail.com>
2021-08-20 18:29:36 +0900

    C++ Server: Session #1, 공부전략 수정 -> 약간 1순환(?)은 모두 다 이해하려고 하지말고 꼭 타이핑을 하지 않더라도 흐름을 쭉 지켜보고, 필요한 또는 궁금한 내용만 우선적으로 보면서 전체 큰 틀을 잡고 나서, 그리고 결과물이 어떤 식으로 나오는지 확인하고 나서 다시 내용 따라가볼 것

--- cpp_server/Server/ServerCore/Listener.cpp
+++ cpp_server/Server/ServerCore/Listener.cpp
@@ -5,6 +5,10 @@
 #include "Session.h"
 #include "Service.h"
 
+/*--------------
+	Listener
+---------------*/
+
 Listener::~Listener()
 {
 	SocketUtils::Close(_socket);
@@ -26,13 +30,16 @@ bool Listener::StartAccept(ServerServiceRef service)
 	_socket = SocketUtils::CreateSocket();
 	if (_socket == INVALID_SOCKET)
 		return false;
-	if (_service->GetIocpCore()->Register(shared_from_this())==false)
+
+	if (_service->GetIocpCore()->Register(shared_from_this()) == false)
 		return false;
+
 	if (SocketUtils::SetReuseAddress(_socket, true) == false)
 		return false;
 
 	if (SocketUtils::SetLinger(_socket, 0, 0) == false)
 		return false;
+
 	if (SocketUtils::Bind(_socket, _service->GetNetAddress()) == false)
 		return false;
 
@@ -43,7 +50,7 @@ bool Listener::StartAccept(ServerServiceRef service)
 	for (int32 i = 0; i < acceptCount; i++)
 	{
 		AcceptEvent* acceptEvent = xnew<AcceptEvent>();
-		acceptEvent->owner = shared_from_this();  //레퍼런스 카운트를 유지한채로 shared_ptr 생성
+		acceptEvent->owner = shared_from_this();
 		_acceptEvents.push_back(acceptEvent);
 		RegisterAccept(acceptEvent);
 	}
@@ -64,23 +71,21 @@ HANDLE Listener::GetHandle()
 void Listener::Dispatch(IocpEvent* iocpEvent, int32 numOfBytes)
 {
 	ASSERT_CRASH(iocpEvent->eventType == EventType::Accept);
-
 	AcceptEvent* acceptEvent = static_cast<AcceptEvent*>(iocpEvent);
 	ProcessAccept(acceptEvent);
 }
 
 void Listener::RegisterAccept(AcceptEvent* acceptEvent)
 {
-	SessionRef session = _service->CreateSession(); //Register IOCP
-	
+	SessionRef session = _service->CreateSession(); // Register IOCP
+
 	acceptEvent->Init();
 	acceptEvent->session = session;
 
 	DWORD bytesReceived = 0;
-	if (false == SocketUtils::AcceptEx(_socket, session->GetSocket(), session->_recvBuffer, 0,
-		sizeof(SOCKADDR_IN) + 16, sizeof(SOCKADDR_IN) + 16, OUT & bytesReceived, static_cast<LPOVERLAPPED>(acceptEvent)))
+	if (false == SocketUtils::AcceptEx(_socket, session->GetSocket(), session->_recvBuffer, 0, sizeof(SOCKADDR_IN) + 16, sizeof(SOCKADDR_IN) + 16, OUT & bytesReceived, static_cast<LPOVERLAPPED>(acceptEvent)))
 	{
-		const int32 errorCode = WSAGetLastError();
+		const int32 errorCode = ::WSAGetLastError();
 		if (errorCode != WSA_IO_PENDING)
 		{
 			// 일단 다시 Accept 걸어준다
@@ -98,19 +103,16 @@ void Listener::ProcessAccept(AcceptEvent* acceptEvent)
 		RegisterAccept(acceptEvent);
 		return;
 	}
+
 	SOCKADDR_IN sockAddress;
 	int32 sizeOfSockAddr = sizeof(sockAddress);
 	if (SOCKET_ERROR == ::getpeername(session->GetSocket(), OUT reinterpret_cast<SOCKADDR*>(&sockAddress), &sizeOfSockAddr))
 	{
 		RegisterAccept(acceptEvent);
-		return ;
+		return;
 	}
 
 	session->SetNetAddress(NetAddress(sockAddress));
-
-	cout << "Client Connected! " << endl;
-
-	// TODO
-
+	session->ProcessConnect();
 	RegisterAccept(acceptEvent);
 }
