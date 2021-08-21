11c1a7bac77473e4b6f1d8df9a4a61e402049bb7
yongbbbba <lisagamer123@gmail.com>
2021-08-21 12:22:38 +0900

    C# Server: C# 서버 강의 수강 재시작. C++ 강의는 좀 더 방대한 내용을 다루는데, 시간도 길어서 큰 틀을 이해하기가 어려운 부분이 있었다. 특히 앞에 내용 까먹어서.. 그래서 시간이 더 짧은 C# 강의를 빠르게 이번 주말 내에 보고 큰틀을 잡고, 서버 개발에 필요한 지식들을 학습한 후에 이를 바탕으로 최흥배님의 IOCP 연습자료를 공부해서 채팅 서버를 만들어볼 계획이다

---
+++ CSharp_server/Server/ServerCore/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+// TLS를 사용하면 JobQueue를 사용할 때 Lock을 걸 필요가 없고 그 스레드에 할당된 Task만 처리하면 되니까 효율적이다.
+
+namespace ServerCore
+{
+    class Program
+    {
+        static ThreadLocal<string> ThreadName = new ThreadLocal<string>(()=> { return $"My name is {Thread.CurrentThread.ManagedThreadId}"; });
+
+        static void WhoAmI()
+        {
+            bool repeat = ThreadName.IsValueCreated;
+            if (repeat)
+                Console.WriteLine(ThreadName.Value + " (repeat)");
+            else
+                Console.WriteLine(ThreadName.Value);
+        }
+
+
+        static void Main(string[] args)
+        {
+            ThreadPool.SetMinThreads(1, 1);
+            ThreadPool.SetMaxThreads(3,, 3);
+            Parallel.Invoke(WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI);
+        }
+    }
+}
